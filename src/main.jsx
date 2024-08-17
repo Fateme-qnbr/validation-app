9e5a6996a0dbfb83c86b087e4fedd8dbe738ba72
Fateme_qnbr <fatemeqanbari010@gmail.com>
2024-08-17 09:06:21 +0330

    f1:adding-{t-tailwind}-vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './input.css'
+import './index.css'
 
-// import './index.css'
-import './output.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-    hi im marry 
-  </h1>
   </StrictMode>,
 )
