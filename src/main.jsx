58c042561d22ecbfbf83288761fcfb4bf6160744
Fateme_qnbr <fatemeqanbari010@gmail.com>
2024-08-17 08:17:34 +0330

    f1-{tailwindcss}-adding

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './input.css'
 
+// import './index.css'
+import './output.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+    hi im marry 
+  </h1>
   </StrictMode>,
 )
