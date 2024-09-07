355abb0c4142ca87fa4cc63a7a2cfe2d4df8d6a9
Fateme_qnbr <fatemeqanbari010@gmail.com>
2024-09-07 09:36:19 +0330

    F2-rs: HomePage-2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 
 export default function App() {
   return (
-    <div class="h-screen bg-custom-orange">
+    <div class="sm:h-screen h-full bg-custom-orange">
       <div class="text-2xl tailwind-menu z-50 sticky top-0 w-full p-4 bg-custom-orange rounded shadow">
         <Navbar />
       </div>
